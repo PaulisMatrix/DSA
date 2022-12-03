91cb3cfb0938969c5a81166326cf3b1eebc8b20e
PaulisMatrix <1999.yadwade@gmail.com>
2022-12-03 11:14:45 +0530

    aoc'22 day3

---
+++ adventofcode/AOC_2022/Day3/first_part.py
@@ -0,0 +1,15 @@
+scores = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26, 
+'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39, 'N': 40, 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45, 'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52}
+
+def solution(file):
+    result = 0
+    with open(file) as file:
+        for line in file:
+            line_len = len(line)
+            first_half,second_half = set(line.strip()[:line_len//2]), set(line.strip()[line_len//2:])
+            result+=scores[list(first_half.intersection(second_half))[0]]
+
+    print(result)
+if __name__ == "__main__":
+    file = "input.txt"
+    solution(file)
